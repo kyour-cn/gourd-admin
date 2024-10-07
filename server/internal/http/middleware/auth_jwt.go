3b68451464e1101710558bc1bac63fe3e079d896
Kyour <kyour@outlook.com>
2024-10-07 15:45:04 +0800

    perf(auth): 优化鉴权中间件代码

--- server/internal/http/middleware/auth_jwt.go
+++ server/internal/http/middleware/auth_jwt.go
@@ -8,25 +8,28 @@ import (
 	"net/http"
 )
 
-// AuthJwtMiddleware 验证 JWT token 并获取用户信息
+// AuthJwtMiddleware JWT鉴权中间件
+// 验证JWT token并获取用户信息
 func AuthJwtMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 从请求头中获取 JWT token
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
 			http.Error(w, "Authorization header is required", http.StatusUnauthorized)
 			return
 		}
-
 		headerToken := authHeader[len("Bearer "):]
 
+		// 获取JWT配置
 		conf, err := config.GetJwtConfig()
 		if err != nil {
 			http.Error(w, "Internal Server Error: No JWT config found", http.StatusInternalServerError)
 			return
 		}
 
+		// 解析 JWT token到Claims
 		jwtData := service.UserClaims{}
-		token, err := jwt.ParseWithClaims(headerToken, &jwtData, func(token *jwt.Token) (interface{}, error) {
+		token, err := jwt.ParseWithClaims(headerToken, &jwtData, func(token *jwt.Token) (any, error) {
 			return []byte(conf.Secret), nil
 		})
 		if err != nil {
@@ -40,7 +43,7 @@ func AuthJwtMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		// 验证 JWT token 权限
+		// 验证 JWT token 的接口权限
 		if !service.CheckJwtPermission(jwtData, r) {
 			http.Error(w, "Forbidden", http.StatusForbidden)
 			return
