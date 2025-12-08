24fd80df4c2cd768c164d310606b1024bdf5f081
Kyour <kyour@outlook.com>
2025-12-08 14:28:56 +0800

    refactor: 移除重复的json编码注册函数并更新错误处理

--- server/cmd/gorm/methods/json_method.go
+++ server/cmd/gorm/methods/json_method.go
@@ -1,47 +1,6 @@
 package methods
 
-import (
-	"time"
-	"unsafe"
-
-	"github.com/json-iterator/go"
-	"gorm.io/gorm"
-)
-
-func init() {
-
-	// 注册全局 encoder/empty 判定
-	// 主要用来处理 time.Time 类型的零值，后期建议使用json/v2替换
-	jsoniter.RegisterTypeEncoderFunc("time.Time",
-		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
-			t := *((*time.Time)(ptr))
-			if t.IsZero() {
-				stream.WriteString("")
-				return
-			}
-			stream.WriteString(t.Format("2006-01-02 15:04:05"))
-		},
-		func(ptr unsafe.Pointer) bool {
-			t := *((*time.Time)(ptr))
-			return t.IsZero()
-		},
-	)
-	jsoniter.RegisterTypeEncoderFunc("gorm.DeletedAt",
-		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
-			t := *((*gorm.DeletedAt)(ptr))
-			if !t.Valid {
-				stream.WriteString("")
-				return
-			}
-			stream.WriteString(t.Time.Format("2006-01-02 15:04:05"))
-		},
-		func(ptr unsafe.Pointer) bool {
-			t := *((*time.Time)(ptr))
-			return t.IsZero()
-		},
-	)
-
-}
+import "github.com/json-iterator/go"
 
 // JsonMethod 实现json序列化、反序列化接口方法
 type JsonMethod struct {
--- server/internal/initialize/init_common.go
+++ server/internal/initialize/init_common.go
@@ -3,16 +3,60 @@ package initialize
 import (
 	"app/internal/util/cache"
 	"context"
+	"time"
+	"unsafe"
+
+	jsoniter "github.com/json-iterator/go"
+	"gorm.io/gorm"
 )
 
-func InitCommon(ctx context.Context) {
+func InitCommon(ctx context.Context) error {
+
+	// 注册全局 encoder/empty 判定
+	// 主要用来处理 time.Time 类型的零值，后期建议使用json/v2替换
+	jsoniter.RegisterTypeEncoderFunc("time.Time",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			t := *((*time.Time)(ptr))
+			if t.IsZero() {
+				stream.WriteString("")
+				return
+			}
+			stream.WriteString(t.Format("2006-01-02 15:04:05"))
+		},
+		func(ptr unsafe.Pointer) bool {
+			t := *((*time.Time)(ptr))
+			return t.IsZero()
+		},
+	)
+	jsoniter.RegisterTypeEncoderFunc("gorm.DeletedAt",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			t := *((*gorm.DeletedAt)(ptr))
+			if !t.Valid {
+				stream.WriteString("")
+				return
+			}
+			stream.WriteString(t.Time.Format("2006-01-02 15:04:05"))
+		},
+		func(ptr unsafe.Pointer) bool {
+			t := *((*time.Time)(ptr))
+			return t.IsZero()
+		},
+	)
 
 	// 初始化日志
 	err := InitLog()
 	if err != nil {
-		panic(err)
+		return err
+	}
+
+	// 初始化数据库
+	err = InitDatabase()
+	if err != nil {
+		return err
 	}
 
 	// 初始化缓存
 	cache.InitDefaultCache(ctx)
+
+	return nil
 }
