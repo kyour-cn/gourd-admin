e475e2e59b5f89bcbf80284084d768c681b8ae16
Kyour <kyour@outlook.com>
2025-12-23 16:17:48 +0800

    refactor(http): 移除未使用导入并优化错误处理

--- server/internal/http/common/controller/base.go
+++ server/internal/http/common/controller/base.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"errors"
 	"net/http"
-	"strconv"
 
 	"github.com/go-playground/form/v4"
 	"github.com/gookit/validate"
@@ -13,40 +12,37 @@ import (
 	"app/internal/http/common/dto"
 )
 
+// formDecoder use a single instance of Decoder, it caches struct info
 var formDecoder = form.NewDecoder()
 
-// Base 基础控制器
-// 所有控制器都应继承此控制器，可以在此控制器中定义公共方法
-type Base struct{}
-
+// Response 响应体
 type Response struct {
 	Code    int    `json:"code"`
 	Data    any    `json:"data"`
 	Message string `json:"message"`
 }
 
+// Base 基础控制器
+type Base struct{}
+
 // Success 成功时响应
 func (*Base) Success(w http.ResponseWriter, message string, data any) (err error) {
-	if message == "" {
-		message = "success"
-	}
 	res := Response{
-		Code:    0,
 		Data:    data,
 		Message: message,
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	str, _ := json.Marshal(res)
+	str, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
 	_, err = w.Write(str)
 	return
 }
 
 // Fail 失败响应
 func (*Base) Fail(w http.ResponseWriter, code int, message string, data any) (err error) {
-	if message == "" {
-		message = "fail"
-	}
 	res := Response{
 		Code:    code,
 		Data:    data,
@@ -54,7 +50,10 @@ func (*Base) Fail(w http.ResponseWriter, code int, message string, data any) (er
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	str, _ := json.Marshal(res)
+	str, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
 	_, err = w.Write(str)
 	return
 }
@@ -107,19 +106,6 @@ func (*Base) QueryReqUnmarshal(r *http.Request, req any) error {
 		}
 	}
 	return nil
-}
-
-// PageParam 获取分页参数
-func (*Base) PageParam(r *http.Request, defaultPage int, defaultSize int) (int, int) {
-	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
-	if page == 0 {
-		page = defaultPage
-	}
-	pageSize, _ := strconv.Atoi(r.URL.Query().Get("page_size"))
-	if pageSize == 0 {
-		pageSize = defaultSize
-	}
-	return page, pageSize
 }
 
 // GetJwt 从请求上下文中获取JWT信息
